a90e995a4d6a6be5c4a8505442c58c95e7ce7279
Parrot Live user <user@localhost.localdomain>
2020-08-13 14:31:24 +0000

    adding on more

---
+++ python.py
@@ -0,0 +1,24 @@
+name="Arun Vijay"
+print(name)
+a =10
+print("hello how are you ",+a)
+print("hello "+str(a))
+print("Inputing The Value")
+
+print("Enter Three Number To find the average")
+c=int(input())
+d=int(input())
+e=int(input())
+f=c+d+e/3
+print("average of three is",+f)
+
+print("swapping of two")
+x=55
+y=66
+t=x
+x=y
+y=t
+print("this is the vlaue of x "+str(x)+ "and value of y is"+str(y))
+
+g = "this is how many"
+print(len(g))
