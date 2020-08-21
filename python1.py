d7d72b30bf036ff14bca2c53339a3f8507e42e4b
Parrot Live user <user@localhost.localdomain>
2020-08-21 11:32:07 +0000

    python extra basic under dev

--- python1.py
+++ python1.py
@@ -20,3 +20,16 @@ print("\nThis is a string now \t" +str(integer));
 print("\nReversing The String");
 rev=("This whole line has to be reversed");
 print(rev[::-1]);
+
+print("\n Multiline String to Print use three codes");
+multi="""This 
+is a multi
+line string 
+that i am writing here"""
+print(multi);
+print("This is the length of a string\t" +str(len(multi)));
+
+
+print("\nRemoving the white space in a string");
+empty =("    this is a space     we have to remove it    so we use strip       ");
+print( empty.strip() );
