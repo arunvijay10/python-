7b4f5fd306be65c96917c34cfcb59a0c26df33ee
Parrot Live user <user@localhost.localdomain>
2020-08-21 12:20:41 +0000

    under dev python

--- extra.py
+++ extra.py
@@ -6,3 +6,4 @@ print("""Three Colons are used to write multi lines """) #multilines
 print("Repace this line we have to pass two parameters") #Replace
 print("string formatter line  55 we  use {}) #format
 print("Finding the index we will use index) #index
+print("finding the upper lower ascii by using is.upper()) #finding
