2cfa0b9bba31d1c93dfafd677e0ffebaa465cb0e
Parrot Live user <user@localhost.localdomain>
2020-08-21 11:22:33 +0000

    python extra basic under dev

---
+++ python1.py
@@ -0,0 +1,22 @@
+print("This is a type casting of a number");
+x=4.5;
+print(int(x));
+
+print("\nThis is a type casting to float\n ");
+z=85;
+print(float(z))
+
+print("\n concatnation of a string\n");
+
+str1=("This is a string to be concatenate");
+str2=("This is the next string to be concatenate with first");
+print("\n"+str1+str2);
+
+
+print("\nconverting integer into a string");
+integer = 45;
+print("\nThis is a string now \t" +str(integer));
+
+print("\nReversing The String");
+rev=("This whole line has to be reversed");
+print(rev[::-1]);
