5ad33af6c25426044f60e844108d35162731ae3f
Parrot Live user <user@localhost.localdomain>
2020-08-22 14:31:36 +0000

    basic

--- python1.py
+++ python1.py
@@ -254,3 +254,21 @@ for i in user17:
     if i =="arun Vijay":
         break
     print(i)    
+
+
+
+print("\nRange")
+for user18 in range(20):
+    print(str(user18)+" "+"This is a Range")
+
+print("\nFunction")
+def fun():
+    print("This is a function")
+fun();
+
+def game():
+    user19=int(input("Enter First Number:\t"))
+    user20=int(input("Enter Second Number:\t"))
+    user30=user19+user20
+    print("This is your answer\t:"+str(user30))
+game();
--- test.py
+++ test.py
@@ -1,12 +1,11 @@
-print("\nContinue And Break")
-user16=["arun Vijay","Programmer","Hacker"]
-for i in user16:
-    if i=="arun Vijay":
-        continue
-    print(i)
+print("\nFunction")
+def fun():
+    print("This is a function")
+fun();
 
-user17=["arun Vijay","coder","hacker"]
-for i in user17:
-    if i =="arun Vijay":
-        break
-    print(i)    
+def game():
+    user19=int(input("Enter First Number:\t"))
+    user20=int(input("Enter Second Number:\t"))
+    user30=user19+user20
+    print("This is your answer\t:"+str(user30))
+game();
